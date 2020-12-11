3c1926ae761de4778bb237c6bf7c443bef97ba7c
pulkit99dev <pulkitnagpal987654321@gmail.com>
2020-12-12 01:28:15 +0530

    Setup Database

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,10 +4,10 @@ mongoose.connect('mongodb://localhost/learning');
 
 let db = mongoose.connection;
 
-db.on('err', function(err){
-    if(err){console.log(`Failed to connect to db`)};
-})
+db.on('err', console.error.bind(console, 'Failed to connect to db'));
 
 db.once('open', function(){
     console.log('Connected to db');
 })
+
+module.exports =db;
