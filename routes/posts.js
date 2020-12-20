f82279b3522c6ed4c3480cd7046c32bd900f478f
pulkit99dev <pulkitnagpal987654321@gmail.com>
2020-12-20 23:07:06 +0530

    authentication for posts forms

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,9 @@
 const express = require('express')
-
+const passport = require('passport')
 let router = express.Router();
 
 let postController = require('../controller/post_controller');
 
-router.post('/create', postController.create);
+router.post('/create', passport.checkAuthentication , postController.create);
 
 module.exports = router;
