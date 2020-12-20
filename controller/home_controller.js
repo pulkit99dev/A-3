8ad71500fd5195af762a737cde198e5893755907
pulkit99dev <pulkitnagpal987654321@gmail.com>
2020-12-20 22:38:10 +0530

    rendering posts

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,9 +1,23 @@
+const Post = require("../models/post");
+const { post } = require("../routes");
+
 module.exports.home = function(req, res){
 
     //console.log(req.cookies);
     //res.cookie('new', 10)
+//     Post.find({}, function(err, posts){
+//         if(err){console.log(`Error while finding`)}
+//         return res.render('home', {
+//         title : 'A-3',
+//         posts : posts
+//     });
+// })
 
-    return res.render('home', {
-        title : 'Learning Partial & Layouts'
+    Post.find({}).populate('user').exec(function(err, posts){
+        if(err){console.log(`Error while finding`)}
+        return res.render('home', {
+        title : 'A-3',
+        posts : posts
     });
+    })
 }
