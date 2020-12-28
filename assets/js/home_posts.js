f71d2d7437489451462a2cdb0dda0f4e6b2dc01b
pulkit99dev <pulkitnagpal987654321@gmail.com>
2020-12-28 19:08:22 +0530

    recieving post-form data with Jquery

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -10,13 +10,45 @@
             url : '/posts/create',
             data : newPostForm.serialize(),
             success : function(data){
-                console.log(data);
+                let newPost = newPostDom(data.data.post);
+                $('#post-list-container>ul').prepend(newPost)
             }, error: function(error){
                 console.log(error.responseText)
             }
 
         })
         })
+    }
+
+    //dom
+    let newPostDom = function(post){
+        return $(`
+                    <li id="post-${post._id}">
+                <p>${post.content}</p>
+                <br>
+                <small>${post.user.name}</small>
+                
+                <p><a class="delete-post-button" href="/posts/destroy/${post.id}">Delete</a></p>
+                
+
+            <div>
+                
+                    <form action="/comments/create" method="POST">
+                        <input type="text" name="content" placeholder="Type Here to add comment..." required>
+                        <input type="hidden" name="post" value="${post._id}" >
+                        <input type="submit" value="Add Comment">
+                    </form>
+                
+                
+                <div class="post-comments">
+                    <ul id="post-comments-${post._id}">
+                    </ul>
+                </div>
+
+            </div>
+
+            </li>
+                    `)
     }
 
     createPost();
